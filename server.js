872cfea52f7363249f770172718fb4b1bb568d76
Devanand <devanand.madhavan@verizon.com>
2015-10-28 05:08:31 -0700

    Travis CI Update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.VCAP_APP_PORT || 8080;
 
 
 app.get('/', function(req,res){
-    res.send("Hello world from NodeJS running on cloudfoundry! <BR/> Process running on port: " + port + " on cf server");
+    res.send("Hello world from NodeJS running on cloudfoundry! <BR/> Process running on port: " + port + " on cf server <BR/>" + "<Br/>Test Travis CI");
 });
 
 app.listen(port);
