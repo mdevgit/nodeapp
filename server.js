e7a6bef4aabfdbbd4d632d83de73f17e0e52a651
mdevgit <devanand.madhavan@verizon.com>
2015-11-11 03:43:41 -0800

    travis yml updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.VCAP_APP_PORT || 8080;
 
 
 app.get('/', function(req,res){
-    res.send("Hello world from NodeJS running on cloudfoundry! <BR/> Process running on port: " + port + " on cf server <BR/>" + "<Br/>Test Travis CI");
+    res.send("Hello world from NodeJS running on cloudfoundry! <BR/> Process running on port: " + port + " on cf server <BR/>" + "<Br/>Test Travis CI<br><Br><br>Response generated @ " + (new Date()).toString('MMM/dd/yyyy hh:mm:SS'));
 });
 
 app.listen(port);
