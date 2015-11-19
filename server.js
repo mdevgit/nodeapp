f6c445e1a36be30262485f4d8537639c9435a473
mdevgit <devanand.madhavan@verizon.com>
2015-11-19 00:24:51 -0800

    Test Commit

--- server.js
+++ server.js
@@ -8,5 +8,8 @@ app.get('/', function(req,res){
     res.send("Hello world from NodeJS running on cloudfoundry! <BR/> Process running on port: " + port + " on cf server <BR/>" + "<Br/>Test Travis CI<br><Br><br>Response generated @ " + (new Date()).toString('MMM/dd/yyyy hh:mm:SS'));
 });
 
+app.get('/Nirmal', function (req, res) {
+    res.send("Hello Nirmal, from NodeJS running on cloudfoundry! <BR/> Process running on port: " + port + " on cf server <BR/>" + "<Br/>Test Travis CI<br><Br><br>Response generated @ " + (new Date()).toString('MMM/dd/yyyy hh:mm:SS'));
+});
 app.listen(port);
 
